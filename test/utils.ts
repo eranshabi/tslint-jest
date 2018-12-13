b9ed9662c716c80cfa56d618b4c545c913d6f6fe
Eran Shabi <eranshabi@gmail.com>
2018-12-14 01:31:57 +0200

    add some boilerplate

---
+++ test/utils.ts
@@ -0,0 +1,29 @@
+import { Configuration, Linter } from "tslint";
+
+// export const lint = (path, fileName) => {
+//   const filePath = path + fileName;
+//   const fileContent = fs.readFileSync(filePath, "utf8");
+//   const linter = new Linter({ fix: false });
+//   const configuration = Configuration.findConfiguration(`${path}tslint.json`, filePath).results;
+//
+//   linter.lint(filePath, fileContent, configuration);
+//   return linter.getResult();
+// };
+
+export const expectExpect = {
+  "defaultSeverity": "error",
+  "jsRules": {},
+  "rules": {
+    "expect-expect": true
+  },
+  "rulesDirectory": "src/rules"
+};
+
+export const lintFileString = (file, config) => {
+  const linter = new Linter({ fix: false });
+  const configuration = Configuration.parseConfigFile(config);
+
+  linter.lint('', file, configuration);
+  return linter.getResult();
+};
+
